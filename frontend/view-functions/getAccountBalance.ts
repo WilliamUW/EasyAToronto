cb695c073a8616d591e36730c676411bee86832d
William Wang <bwilliamwang@gmail.com>
2025-05-15 15:50:52 -0400

    Update getAccountBalance.ts

--- frontend/view-functions/getAccountBalance.ts
+++ frontend/view-functions/getAccountBalance.ts
@@ -1,3 +1,4 @@
+import { MODULE_ADDRESS } from "@/constants";
 import { aptosClient } from "@/utils/aptosClient";
 
 export type AccountAPTBalanceArguments = {
@@ -10,6 +11,21 @@ export const getAccountAPTBalance = async (args: AccountAPTBalanceArguments): Pr
     payload: {
       function: "0x1::coin::balance",
       typeArguments: ["0x1::aptos_coin::AptosCoin"],
+      functionArguments: [accountAddress],
+    },
+  });
+  return balance[0];
+};
+
+export type AccountBBTBalanceArguments = {
+  accountAddress: string;
+};
+
+export const getAccountBBTBalance = async (args: AccountBBTBalanceArguments): Promise<number> => {
+  const { accountAddress } = args;
+  const balance = await aptosClient().view<[number]>({
+    payload: {
+      function: `${MODULE_ADDRESS}::behavioral_buddy::get_balance`,
       functionArguments: [accountAddress],
     },
   });
